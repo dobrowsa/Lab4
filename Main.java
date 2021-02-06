f2db10ee32b10526ab33557fe7132f99eb3f4b44
dobrowsa <dobrowsa@mail.uc.edu>
2021-02-06 02:22:18 +0000

    Added Comments for Code, and this is attempt one at Group part of code for Monday's discussion with Dan.

--- Main.java
+++ Main.java
@@ -1,18 +1,21 @@
+//Importing Random Util
 import java.util.Random;
 
-import javax.lang.model.util.ElementScanner6;
-
 class Main {
+  //assigning random Variable
   public static void main(String[] args) {
   Random r = new Random();
   int randomNum = r.nextInt(50);  
     System.out.println("The Random number is: " + randomNum);
   
+  //Creating substitute variable
   int countdown = randomNum;
 
+  // Countdown from Random Number Input
   for (countdown = randomNum; countdown>=0; countdown--) {
       System.out.println(countdown);
     }
+  // If/else statement specifying different print messages  
   if (randomNum <= 5){
     System.out.println("Ahoy mateys!");
   }
